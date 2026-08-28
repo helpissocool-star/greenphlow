a7b1cadbefcfd5ffb5402801f3f78bd71abb29af
helpissocool-star <helpissocool@gmail.com>
2026-08-28 22:15:42 +0800

    Update map.js

--- js/map.js
+++ js/map.js
@@ -12,7 +12,7 @@ const FIELDS = {
   siteArea: "area_sqm",
   floodScore: "flood_attenuation_score_x",
   siteCost: "site_cost_x",
-  funded: "selection_x",            
+  funded: "selection_y",            
   barangay: "brgy_name",          
 };
 
